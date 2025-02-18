11b04ec6b1ecb65044714af86cde368d996320a5
Lvhacker1 <lavasha2@hotmail.com>
2025-02-18 16:35:19 +0100

    Add HTML, CSS, and jQuery API calls for jokes

---
+++ API/scripts/scripts.js
@@ -0,0 +1,40 @@
+$(() => {
+    const API_URL_RANDOM = "https://official-joke-api.appspot.com/jokes/";
+    const API_URL_JOKE = "https://v2.jokeapi.dev/joke/";
+
+    const catchJoke = async () => {
+        try {
+            let response = await fetch(`${API_URL_RANDOM}random`);
+            let jokeData = await response.json();
+
+            $('#joke-result').html(`<p>Question: ${jokeData.setup}</p> <p>Answer: ${jokeData.punchline}</p>`).show();
+            console.log('Random joke:', jokeData);
+        } catch (error) {
+            console.error('Error fetching random joke:', error)
+            $('#joke-result').text('Failed to fetch random joke.').show();
+        }
+    };
+    
+    const getJokeType = async () => {
+        let jokeType = $('#joke-input').val().trim().toLowerCase();
+        console.log('Joketype:', jokeType);
+
+        if (jokeType !== "programming" && jokeType !== "dark") {
+            $('#joke-type_result').text('Invalid category. Type "programming" or "dark".').show();
+            return;
+        }
+        try {
+            let response = await fetch(`${API_URL_JOKE}${jokeType}?type=twopart`);
+            let jokeData = await response.json();
+
+            $('#joke-type_result').html(`<p>Question: ${jokeData.setup}</p> <p>Answer: ${jokeData.delivery}</p>`).show();
+            console.log('Joke type:', jokeData);
+        } catch (error) {
+            console.error('Error fetching joke by category:', error);
+            $('#joke-type_result').text('Failed to fetch joke.').show();
+        }
+    };
+
+    $('#random-joke').on('click', catchJoke)
+    $('#get_joke-type').on('click', getJokeType)
+});
